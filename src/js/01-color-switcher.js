8ce6bc69bc7722f4e905b2ef0b534f4c7b494b79
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2022-05-31 00:14:02 +0200

    add hw-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+let isEnabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+startBtn.addEventListener('click', () => {
+    if (isEnabled === true) {
+        timerId = setInterval(() => {
+            body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+    } else {
+        isEnabled === false;
+    }
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+     isEnabled === true;
+  
+});
