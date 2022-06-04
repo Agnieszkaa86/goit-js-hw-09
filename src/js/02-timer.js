77f941b58bbe4be87c44431d4920001b5d5401b2
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2022-06-04 23:22:16 +0200

    task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,10 +1,11 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-import Notiflix from 'notiflix';
-timerId = 0;
+import Notiflix, { Notify } from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
 const dateTime = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('[data-start]');
-let usersTime;
+
 btnStart.disabled = true;
 
 flatpickr(dateTime, {
@@ -16,48 +17,51 @@ flatpickr(dateTime, {
     if (selectedDates[0] < new Date()) {
       Notiflix.Notify.failure('Please choose a date in the future');
     } else {
-      startBtn.disabled = false;
+      btnStart.disabled = false;
       Notiflix.Notify.success(
-        'Please press "start" to proceed'
+        'Please press "START" to proceed'
       );
     }
-  },
+  }
 });
 
+btnStart.addEventListener('click', () => {
+  setInterval(() => {
+    const currentTime = new Date().getTime();
+    const usersTime = new Date(dateTime.value).getTime();
+    const result = usersTime - currentTime;
 
-function convertMs() {
-  const currentTime = new Date();
-  usersTime = new Date(dateTime.value).getTime();
-  const result = usersTime - currentTime;
+    // Remaining days
+    const days = Math.floor(result / (1000 * 60 * 60 * 24));
+    // Remaining hours
+    const hours = Math.floor((result % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    // Remaining minutes
+    const minutes = Math.floor((result % (1000 * 60 * 60)) / (1000 * 60));
+    // Remaining seconds
+    const seconds = Math.floor((result % (1000 * 60)) / 1000);
 
-  // Remaining days
-  const days = Math.floor(result / (1000 * 60 * 60 * 24));
-  // Remaining hours
-  const hours = Math.floor((result % (1000 * 60 * 60)) / (1000 * 60 * 60));
-  // Remaining minutes
-  const minutes = Math.floor((result % (1000 * 60 * 60)) / (1000 * 60));
-  // Remaining seconds
-  const seconds = Math.floor((result % (1000 * 60)) / 1000);
+    document.querySelector('[data-days]').innerHTML =
+      days < 10 ? '0' + days : days;
+    document.querySelector('[data-hours]').innerHTML =
+      hours < 10 ? '0' + hours : hours;
+    document.querySelector('[data-minutes]').innerHTML =
+      minutes < 10 ? '0' + minutes : minutes;
+    document.querySelector('[data-seconds]').innerHTML =
+      seconds < 10 ? '0' + seconds : seconds;
 
-  document.querySelector('[data-days]').innerHTML =
-    days < 10 ? '0' + days : days;
-  document.querySelector('[data-hours]').innerHTML =
-    hours < 10 ? '0' + hours : hours;
-  document.querySelector('[data-minutes]').innerHTML =
-    minutes < 10 ? '0' + minutes : minutes;
-  document.querySelector('[data-seconds]').innerHTML =
-    seconds < 10 ? '0' + seconds : seconds;
-
-  if (result < 0) {
-    clearInterval(timerId);
-    document.querySelector('[data-days]').innerHTML = '00';
-    document.querySelector('[data-hours]').innerHTML = '00';
-    document.querySelector('[data-minutes]').innerHTML = '00';
-    document.querySelector('[data-seconds]').innerHTML = '00';
-  }
-}
-btnStart.addEventListener('click', flatpickr)
+    
+    if (result < 0) {
+      clearInterval(timerId);
+      document.querySelector('[data-days]').innerHTML = '00';
+      document.querySelector('[data-hours]').innerHTML = '00';
+      document.querySelector('[data-minutes]').innerHTML = '00';
+      document.querySelector('[data-seconds]').innerHTML = '00';
+      Notify.info ('Countdown stop!')
+    }
+  }, 1000);
+});
+  
 
 flatpickr()
-setInterval(currentTime,1000);
+setInterval(convertMs, 1000);
 
