7c7d2e0d6ff55377b07935abb8eed5d7cc7a1a51
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2022-06-09 09:52:34 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,6 +6,7 @@ import 'notiflix/dist/notiflix-3.2.5.min.css';
 
 const dateTime = document.querySelector("#datetime-picker");
 const btnStart = document.querySelector("[data-start]");
+
 const dataDays = document.querySelector("[data-days]");
 const dataHours = document.querySelector("[data-hours]");
 const dataMinutes = document.querySelector("[data-minutes]");
@@ -15,20 +16,19 @@ const dataSeconds = document.querySelector("[data-seconds]");
 let timerId = null;
 btnStart.disabled = true;
 
-const options = {
+const options ={
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-  const selectedDate = selectedDates[0];
-    if (selectedDate > options.defaultDate.getTime()) {
+    usersTime = selectedDates[0];
+    if (usersTime > new Date()) {
       btnStart.disabled = false;
-      Notiflix.Notify.success('Please press "START" to proceed');
-      dateTime.dataset.time = selectedDate.getTime();
+    Notiflix.Notify.success('Please press "START" to proceed');
+      dateTime.dataset.time = usersTime.getTime();
     } else {
-      Notiflix.Notify.failure('Please choose a date in the future');
-      btnStart.disabled = true; 
+      Notiflix.Notify.failure('Please choose a date in the future'); 
    }
   },
 };
@@ -37,21 +37,17 @@ const options = {
 function appUpdate() {
   timerId = setInterval(() => {
     btnStart.disabled = true;
-    let currentTime = new Date().getTime();
-    let usersTime = Number(dateTime.dataset.time);
-    const result = usersTime - currentTime;
-    console.log(result);
-    // Remaining days
+    const currentTime = new Date().getTime();
+    let countTime = Number(dateTime.dataset.time);
+    const result = countTime - currentTime;
+
     const days = Math.floor(result / (1000 * 60 * 60 * 24));
-    // Remaining hours
     const hours = Math.floor(
       (result % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
     );
-    // Remaining minutes
     const minutes = Math.floor((result % (1000 * 60 * 60)) / (1000 * 60));
-    // Remaining seconds
     const seconds = Math.floor((result % (1000 * 60)) / 1000);
-  
+
     dataDays.innerHTML = days < 10 ? '0' + days : days;
     dataHours.innerHTML = hours < 10 ? '0' + hours : hours;
     dataMinutes.innerHTML = minutes < 10 ? '0' + minutes : minutes;
